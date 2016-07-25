d071819b6c6eeac8c774d6e35165c9981d12ad5d
finn1990 <fall1600@gmail.com>
2016-07-25 13:44:47 +0800

    use opacity to hide and show right, bottom div

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,54 +4,93 @@
  * and open the template in the editor.
  */
 $(document).ready(function () {
-  
+
   $("#root").css("background-color", getRandomColor())
     .css("z-index", 0);
   $("#divRight").css("background-color", getRandomColor())
-    .css("z-index", 1);
+    .css("z-index", 1).css("opacity", 0);
   $("#divBottom ").css("background-color", getRandomColor())
-    .css("z-index", 1);;
-  
-  $(".rectangle").on("click", function() {
+    .css("z-index", 1).css("opacity", 0);
+
+  $(".rectangle").on("click", function () {
     var thisRec = $(this);
-    console.log(thisRec.width());
-    console.log(thisRec.height());
-    console.log(thisRec.css("z-index"));
+    var thisWidth,
+      thisHeight,
+      thisZindex;
+
+    thisWidth = thisRec.width();
+    thisHeight = thisRec.height();
+    thisZindex = thisRec.css("z-index");
+
+    alert(thisWidth + " " + thisHeight + " " + thisZindex);
+
   });
-  
-   $("#divRight").on("click", function() {
+
+  $("#divRight").hover(
+    function () {
+      $(this).fadeTo(1, 1);
+    }, function () {
+    $(this).fadeTo(1, 0);
+  }
+  );
+
+  $("#divBottom").hover(
+    function () {
+      $(this).fadeTo(1, 1);
+    }, function () {
+    $(this).fadeTo(1, 0);
+  }
+  );
+
+  $("#divRight").on("click", function (event) {
+    event.stopPropagation();
     var parent = $(this).parent();
     console.log(parent.width());
     console.log(parent.height());
-    
+
     var thisRec = $(this);
-    console.log(thisRec.css("z-index"));
+    var thisWidth,
+      thisHeight,
+      thisZindex;
+    thisWidth = thisRec.width();
+    thisHeight = thisRec.height();
+    thisZindex = thisRec.css("z-index");
+
+    alert(thisWidth + " " + thisHeight + " " + thisZindex);
   });
-  
-   $("#divBottom").on("click", function() {
+
+  $("#divBottom").on("click", function (event) {
+    event.stopPropagation();
     var parent = $(this).parent();
     console.log(parent.width());
     console.log(parent.height());
-    
+
     var thisRec = $(this);
-    console.log(thisRec.css("z-index"));
+    var thisWidth,
+      thisHeight,
+      thisZindex;
+    thisWidth = thisRec.width();
+    thisHeight = thisRec.height();
+    thisZindex = thisRec.css("z-index");
+
+    alert(thisWidth + " " + thisHeight + " " + thisZindex);
+
   });
-  
-  $("#right").on("click", function() {
+
+  $("#right").on("click", function () {
     alert("right");
   });
-  
-  $("#bottom").on("click", function() {
+
+  $("#bottom").on("click", function () {
     alert("bottom");
   });
-  
+
   function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
   }
 });
-
